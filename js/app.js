eaa64d21b8934bab3325184c257a6f1f19643f09
papanugget <evilgenius04@gmail.com>
2018-05-08 18:52:32 -0400

    removed unneeded classes from index, refactored buildCards function because it was duplicating some icons

--- js/app.js
+++ js/app.js
@@ -2,49 +2,68 @@
  * Create a list that holds all of your cards
  */
 
-// let cards = [];
-// //select card elements
-// cards = document.querySelectorAll('.card')
-//init empty array for i class values
-
-//loop thru cards and select i class values and push into empty array
-
-    // console.log(cardItems);
-
 //would it be easier to just have a hard coded array to hold the i class values?
 const cardArr = ["fa fa-bicycle", "fa fa-bolt", "fa fa-bomb", "fa fa-paper-plane-o", "fa fa-bomb", "fa fa-bolt", "fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-bicycle", "fa fa-cube", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-leaf", "fa fa-anchor"];
 
+// function buildCards(){
+//     //init empty array to hold card elements
+//     let cards = [];
+//     //select card elements
+//     cards = document.querySelectorAll('.card');
+//     //loop thru cards and remove icons
+//     for(let i = 0; i < cards.length; i++){
+//         //clear old icons
+//         cards[i].innerHTML = '';
+//         //loop thru shuffed card array
+//         for(let j = 0; j < cardArr.length; j++){
+            
+//             // let icon = '';
+//             // icon += `<i class="${cardArr[j]}"></i>`;
+//             // cards[i].innerHTML += icon;
+//             console.log(cardArr[j]);
+//         }
+//         // console.log(cards);
+//     }
+// }
 function buildCards(){
-    //clear icons by selecting cards
-    //init empty array to hold card elements
-    let cards = [];
-    //select card elements
-    cards = document.querySelectorAll('.card');
-    //loop thru cards and remove icons
-    for(let i = 0; i < cards.length; i++){
+    let cards = document.querySelectorAll('.card');
+    // console.log(cards);
+    //loop thru shuffled cardArr
+    for(let i = 0; i < cardArr.length; i++){
+        //clear icons
         cards[i].innerHTML = '';
-        // //shuffle hardcoded cardArr
-        shuffle(cardArr);
-        //loop thru shuffed card array
-        for(let j = 0; j < cardArr.length; j++){
-            //each card will have new card icon
-            //build html using template literals
-            //insert them into each card element
-            cards[i].innerHTML = `<i class="${cardArr[j]}"></i>`;
-        }
-        console.table(cards);
+        //set card icons with each from shuffled cardArr
+        cards[i].innerHTML = `<i class="${cardArr[i]}"></i>`;
     }
+    // console.log(cards);
+}       
+//     console.log(cards);
     
-    // console.log(cardArr);
-    //loop thru shuffled array
-    // cardArr.forEach((icon) => {
-    //     // console.log(icon);
-    //     output = `<i class="${icon}></i>"`
-    //     for(let j = 0; j < cards.length; j++){
-    //         cards[j].innerHTML += output;
-    //     }
-    // });
-}
+//     // console.log(cardArr);
+//     //loop thru shuffled array
+//     // cardArr.forEach((icon) => {
+//     //     // console.log(icon);
+//     //     output = `<i class="${icon}></i>"`
+//     //     for(let j = 0; j < cards.length; j++){
+//     //         cards[j].innerHTML += output;
+//     //     }
+//     // });
+// }
+
+//rebuild buildCards function with forEach loops
+// function buildCards(){
+//     let cards = [];
+//     shuffle(cardArr);
+//     console.table(cardArr);
+//     cards = document.querySelectorAll('.card');
+//     cards.forEach((card) => {
+//        card.innerHTML = '';
+//         cardArr.forEach((icon) => {
+//         card.innerHTML += `<i class="${icon}"></i>`;
+//         });
+//     });  
+//     console.log(cards);  
+// }
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -66,8 +85,12 @@ function shuffle(array) {
 
     return array;
 }
-
+// //shuffle hardcoded cardArr
+shuffle(cardArr);
+console.log(cardArr);
+//randomize card icon positions on page load
 buildCards();
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
